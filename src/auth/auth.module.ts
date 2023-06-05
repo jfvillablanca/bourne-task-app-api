13331aa6dabcd405e3a655eaccd6d93ef6bbff07
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-05 20:46:05 +0800

    Refactor: use relative path on imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/user/user.model';
+import { User, UserSchema } from '../user/user.model';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/user/user.model';
+import { User } from '../user/user.model';
 import { AuthDto } from './dto';
 
 @Injectable()
