bf3b96e3758109aa96073ef0d24cea9e221e3bd5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-05 17:21:26 +0800

    Feat: set connection to local mongodb database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+const connectionString = 'mongodb://localhost:27017/bourne-task-app';
+
 @Module({
-    imports: [AuthModule],
+    imports: [MongooseModule.forRoot(connectionString), AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
