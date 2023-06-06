c4e4f05403d218e3dcee2a125c70896f2a506172
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-06 09:43:53 +0800

    Feat: check if username and email are unique

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import * as argon from 'argon2';
 import { Model } from 'mongoose';
 import { User } from '../user/user.model';
 import { AuthDto } from './dto';
-import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -12,6 +12,18 @@ export class AuthService {
     ) {}
 
     async register(dto: AuthDto) {
+        const usernameTaken = await this.userModel.exists({
+            username: dto.username,
+        });
+        if (usernameTaken) {
+            throw new ConflictException('Username is already taken');
+        }
+
+        const emailTaken = await this.userModel.exists({ email: dto.email });
+        if (emailTaken && dto.email) {
+            throw new ConflictException('Email is already taken');
+        }
+
         const hash = await argon.hash(dto.password);
         const newUser = new this.userModel({ ...dto, password: hash });
         const document = await newUser.save();
