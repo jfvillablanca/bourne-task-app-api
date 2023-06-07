9533fd23107639b026b8c6b27f6d1db4db13b7b9
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-07 13:30:03 +0800

    Fix: change mongodb port to 27018

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
-const connectionString = 'mongodb://localhost:27017/bourne-task-app';
+const connectionString = 'mongodb://localhost:27018/bourne-task-app';
 
 @Module({
     imports: [MongooseModule.forRoot(connectionString), AuthModule],
