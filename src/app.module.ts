043c65245f00182154c00961f6a11cb7f4300bd7
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-10 06:48:01 +0800

    Chore: generate new crud resource: project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { mongooseConfig } from './config/database.config';
 import { UserController } from './user/user.controller';
+import { ProjectModule } from './project/project.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         MongooseModule.forRootAsync(mongooseConfig()),
         AuthModule,
+        ProjectModule,
     ],
     controllers: [UserController],
 })
