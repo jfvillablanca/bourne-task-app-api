71e125e8bebc80068bd4e8ab847c234623b14c77
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-09 20:32:01 +0800

    Test(e2e): setup integration testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { getConnectionToken } from '@nestjs/mongoose';
+import { Test } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
+import { LoginDTOStub, RegisterDTOStub } from './stubs/auth.dto.stub';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
+    let mongoConnection: Connection;
+    const port = 3333;
+    const baseUrl = `http://localhost:${port}`;
 
-    beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
+    beforeAll(async () => {
+        const moduleFixture = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+            }),
+        );
         await app.init();
+        await app.listen(port);
+
+        mongoConnection = await app.resolve(getConnectionToken());
+
+        pactum.request.setBaseUrl(baseUrl);
     });
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!');
+    afterAll(() => {
+        app.close();
+    });
+
+    afterEach(async () => {
+        const collections = mongoConnection.collections;
+        for (const key in collections) {
+            const collection = collections[key];
+            await collection.deleteMany({});
+        }
+    });
+
     });
 });
