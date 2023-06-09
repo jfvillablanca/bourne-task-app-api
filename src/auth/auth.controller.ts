cf2d6084b18be98b2af2a15e26c2eb7ade87081c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-09 21:11:05 +0800

    Feat: explicitly signify status on auth success

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto';
 
@@ -7,11 +7,13 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('register')
+    @HttpCode(HttpStatus.CREATED)
     async register(@Body() dto: RegisterDto) {
         return await this.authService.register(dto);
     }
 
     @Post('login')
+    @HttpCode(HttpStatus.OK)
     async login(@Body() dto: LoginDto) {
         return await this.authService.login(dto);
     }
