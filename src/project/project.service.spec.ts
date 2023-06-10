0f8557c21431a821ac33f5bc025cd798d1c5b80d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-10 17:16:57 +0800

    Chore(test): setup unit tests for project.service

--- src/project/project.service.spec.ts
+++ src/project/project.service.spec.ts
@@ -1,18 +1,47 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { connect, Connection, Model, Types } from 'mongoose';
+import { Project, ProjectSchema } from './entities';
 import { ProjectService } from './project.service';
 
 describe('ProjectService', () => {
     let service: ProjectService;
+    let mongod: MongoMemoryServer;
+    let mongoConnection: Connection;
+    let projectModel: Model<Project>;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
+        mongod = await MongoMemoryServer.create();
+        const uri = mongod.getUri();
+        mongoConnection = (await connect(uri)).connection;
+        projectModel = mongoConnection.model(Project.name, ProjectSchema);
+
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ProjectService],
+            providers: [
+                ProjectService,
+                {
+                    provide: getModelToken(Project.name),
+                    useValue: projectModel,
+                },
+            ],
         }).compile();
 
         service = module.get<ProjectService>(ProjectService);
     });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+    afterAll(async () => {
+        await mongoConnection.dropDatabase();
+        await mongoConnection.close();
+        await mongod.stop();
     });
+
+    afterEach(async () => {
+        const collections = mongoConnection.collections;
+        for (const key in collections) {
+            const collection = collections[key];
+            await collection.deleteMany({});
+        }
+    });
+
 });
