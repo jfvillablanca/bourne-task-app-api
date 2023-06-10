bd2079fb0791729e68a7103b7e1ab1f0653babfa
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-10 17:09:24 +0800

    Feat: add dto for create-project

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProjectDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateProjectDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+}
