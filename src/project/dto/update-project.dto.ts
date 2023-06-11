57941c7ca3535c082def5591c5a00cf378df4d91
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-11 13:05:10 +0800

    Feat: add 'collaborators' to update-project-dto

--- src/project/dto/update-project.dto.ts
+++ src/project/dto/update-project.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { CreateProjectDto } from './index';
 
 export class UpdateProjectDto extends PartialType(CreateProjectDto) {
@@ -11,4 +11,8 @@ export class UpdateProjectDto extends PartialType(CreateProjectDto) {
     @IsString()
     @IsOptional()
     description?: string;
+
+    @IsArray()
+    @IsOptional()
+    collaborators?: string[];
 }
