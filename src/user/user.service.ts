efaecc7041044444fae61a65031f22e85f9e5004
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-18 07:16:38 +0800

    Feat: add getMe behavior, returns user's details

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities';
 
 @Injectable()
 export class UserService {
+    constructor(
+        @InjectModel(User.name) private readonly userModel: Model<User>,
+    ) {}
+
     async getMe(userId: string) {
-        return Promise.resolve(userId);
+        const user = await this.userModel.findById(userId).exec();
+        if (!user) throw new NotFoundException('User not found');
+        return user;
     }
 }
